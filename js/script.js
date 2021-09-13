1d312654e64a5d67bd5ab8c9fbb92eb8490fb92f
Gustavo Melo <gtvmelo10@gmail.com>
2021-09-13 12:04:48 -0300

    Alterações nas competências.

--- js/script.js
+++ js/script.js
@@ -19,6 +19,10 @@ function removeShowClass(list, showClass){
 
 
 function showNextIndex(){
+    removeShowClass(skillsG1, 'show')
+    removeShowClass(skillsG2, 'show')
+    removeShowClass(balls, 'aceso')
+
     skillsG1[indexPage].classList.add('show')
     skillsG1[indexPage + 1].classList.add('show') 
     skillsG2[indexPage].classList.add('show')
@@ -29,33 +33,25 @@ function showNextIndex(){
 
 
 btnNext.addEventListener('click', () =>{
-    if(indexBalls == balls.length - 1 || indexPage ==  skillsG1.length -2){
+    indexBalls++
+    indexPage+=2
+    
+    if(indexBalls > balls.length - 1 || indexPage > skillsG1.length -2){
         indexBalls = 0
         indexPage = 0
-    }else{
-        indexBalls++
-        indexPage+=2
     }
-
-    removeShowClass(skillsG1, 'show')
-    removeShowClass(skillsG2, 'show')
-    removeShowClass(balls, 'aceso')
 
     showNextIndex()
 })
 
 btnPrevious.addEventListener('click', ()=>{
-    if(indexBalls == 0 || indexPage == 0){
+    indexBalls--
+    indexPage-=2
+
+    if(indexBalls < 0 || indexPage < 0){
         indexBalls = balls.length - 1
         indexPage = skillsG1.length -2
-    }else{
-        indexBalls--
-        indexPage-=2
     }
-
-    removeShowClass(skillsG1, 'show')
-    removeShowClass(skillsG2, 'show')
-    removeShowClass(balls, 'aceso')
 
     showNextIndex()
 })
